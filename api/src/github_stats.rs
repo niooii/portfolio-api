8279b6035ac8ab93757612f4c76ba0031da18902
niooii <wangh3@bxscience.edu>
2024-04-16 01:04:02 -0400

    somewhat working?

---
+++ api/src/github_stats.rs
@@ -0,0 +1,139 @@
+use std::{collections::HashMap, hash::Hash};
+use octocrab::Octocrab;
+
+#[derive(Debug)]
+pub struct GithubStats {
+    octocrab: Octocrab,
+    pub repos: Vec<Repo>,
+}
+
+impl GithubStats {
+    pub async fn new(gh_token: String) -> Result<Self, octocrab::Error> {
+        let mut gh_stats = Self {
+            octocrab: Octocrab::builder().personal_token(gh_token).build()?,
+            repos: Vec::new()
+        };
+
+        gh_stats.refresh().await?;
+
+        Ok(gh_stats)
+    }
+    pub async fn refresh(&mut self) -> Result<(), octocrab::Error> {
+        let repos = self.octocrab
+        .current()
+        .list_repos_for_authenticated_user()
+        .affiliation("owner")
+        .per_page(100)
+        .send()
+        .await?
+        .into_iter()
+        .filter(|repo| repo.fork.unwrap() == false && repo.size.unwrap() != 0)
+        .collect::<Vec<_>>();
+
+
+        let futures = repos.into_iter().map(|repo| {
+            let octocrab_clone = self.octocrab.clone();
+            async move {
+                let owner_login = repo.owner.unwrap().login;
+                let repo_name = repo.name.clone();
+                let is_public = repo.visibility.as_deref().unwrap() == "public";
+                let url = repo.url;
+                let langs = octocrab_clone
+                    .repos(&owner_login, &repo_name)
+                    .list_languages().await?;
+                Ok::<_, octocrab::Error>((repo_name, langs, is_public, url))
+            }        
+        });
+
+        let results: Vec<_> = futures::future::join_all(futures).await;
+        // let results: Vec<HashMap<_, _>> = results.into_iter().filter(|r| r.is_ok()).map(|r| r.unwrap()).collect();
+        
+        let mut repo_info = HashMap::new();
+
+        for result in results {
+            match result {
+                Ok((repo_name, languages, is_public, url)) => {
+                    let mut filtered_languages = languages.clone();
+                    if filtered_languages.contains_key("Dart") {
+                        filtered_languages.retain(|k, _v| k == "Dart");
+                    }
+                    repo_info.insert(
+                        repo_name, 
+                        (filtered_languages.into_iter()
+                        .map(|(name, bytes)| LanguageInfo{name, bytes})
+                        .collect::<Vec<_>>(),
+                        is_public,
+                        url)
+                    );
+                }
+                Err(err) => {
+                    eprintln!("Error processing repository: {:?}", err);
+                }
+            }
+        }
+
+        self.repos = repo_info.into_iter().map(|(repo_name, (langs, is_public, url))| {
+            Repo::new(repo_name, langs, is_public, url.to_string())
+        }).collect();
+
+        Ok(())
+    }
+
+    // actual interesting stuff 
+    pub fn get_total_lang_stats_bytes(&self) -> HashMap<String, i64> {
+        let mut lang_map = HashMap::new();
+
+        for repo in &self.repos {
+            for lang_info in repo.languages() {
+                let count = lang_map.entry(lang_info.name.clone()).or_insert(0);
+                *count += lang_info.bytes;
+                println!("repo {} contributed {} bytes of {}", repo.name, lang_info.bytes, lang_info.name);
+            }
+        }
+
+        lang_map
+    }
+
+    pub fn get_total_lang_stats_percentage(&self) -> HashMap<String, f32> {
+        
+
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+pub struct Repo {
+    name: String,
+    languages: Vec<LanguageInfo>,
+    is_public: bool,
+    url: String,
+}
+
+impl Repo {
+    pub fn new(name: String, languages: Vec<LanguageInfo>, is_public: bool, url: String) -> Self {
+        Self {
+            name,
+            languages,
+            is_public,
+            url,
+        }
+    }
+    pub fn name(&self) -> &String {
+        &self.name
+    }
+    pub fn languages(&self) -> &Vec<LanguageInfo> {
+        &self.languages
+    }
+    pub fn is_public(&self) -> bool {
+        self.is_public
+    }
+    pub fn url(&self) -> &String {
+        &self.url
+    }
+}
+
+#[derive(Debug)]
+pub struct LanguageInfo {
+    pub name: String,
+    pub bytes: i64
+}
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 use octocrab::Octocrab;
 use dotenvy::dotenv;
-use futures::stream::StreamExt;
+mod github_stats;
+use github_stats::GithubStats;
 
 #[tokio::main]
 async fn main() -> octocrab::Result<()> {
@@ -9,49 +10,11 @@ async fn main() -> octocrab::Result<()> {
     dotenv().expect(".env file not found");
 
     let token = std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN env variable is required");
-    let octocrab = Octocrab::builder().personal_token(token).build()?;
+    
+    let mut gh_stats = GithubStats::new(token).await?;
 
-    let repos = octocrab
-        .current()
-        .list_repos_for_authenticated_user()
-        .affiliation("owner,collaborator")
-        .per_page(100)
-        .send()
-        .await?
-        .into_iter()
-        .filter(|repo| repo.fork.unwrap() == false && repo.size.unwrap() != 0)
-        .collect::<Vec<_>>();
-
-    let mut lang_maps = HashMap::new();
-
-    let futures = repos.into_iter().map(|repo| {
-        let octocrab_clone = octocrab.clone();
-        let owner_login = repo.owner.unwrap().login;
-            let repo_name = repo.name.clone();
-            octocrab_clone
-                .repos(&owner_login, &repo_name)
-                .list_languages()
-    });
-
-    let results: Vec<_> = futures::future::join_all(futures).await;
-    let results: HashMap<_, _> = results.into_iter().filter(|r| r.is_ok()).map(|r| r.unwrap()).collect();
-
-    for result in results {
-        match result {
-            Ok((repo_name, languages)) => {
-                let mut filtered_languages = languages.clone(); 
-                if filtered_languages.contains_key("Dart") {
-                    filtered_languages.retain(|k, _v| k == "Dart");
-                }
-                lang_maps.insert(repo_name, filtered_languages);
-            }
-            Err(err) => {
-                eprintln!("Error processing repository: {:?}", err);
-            }
-        }
-    }
-
-    println!("{:?}", lang_maps);
+    let yes = gh_stats.get_total_lang_stats_bytes();
+        println!("{:?}", yes);
 
     Ok(())
 }
