aae69cb3a3a75ab25956dbf5b7e10d899d540a06
niooii <wangh3@bxscience.edu>
2024-04-21 03:04:37 -0400

    cassandra db stuff

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,19 @@
 use std::collections::HashMap;
+use std::time::Duration;
 use octocrab::Octocrab;
 use dotenvy::dotenv;
 mod github_stats;
 use github_stats::GithubStats;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+mod db;
+
+use scylla::cql_to_rust::FromRow;
+use scylla::macros::FromRow;
+use scylla::{Session, SessionBuilder};
+
+use crate::db::global_messages;
+
+static CASSANDRA_NODE_URL: &str = "127.0.0.1:9042";
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
@@ -20,6 +30,21 @@ async fn main() -> anyhow::Result<()> {
 
     let total_percent: f32 = yes.iter().map(|(_name, percent)| *percent).sum();
     println!("{total_percent}");
+
+    let session: Session = SessionBuilder::new()
+    .known_node(CASSANDRA_NODE_URL)
+    .connection_timeout(Duration::from_secs(10))
+    .build()
+    .await
+    .expect("connect");
+
+    println!("{:?}", session.use_keyspace("misc", false).await);
+    println!("{:?}", session.get_keyspace());
+
+    global_messages::insert_new(&session, &"yes".to_string(), &"TESTMSG".to_string()).await?;
+
+    let messages = global_messages::get_amount(&session, 2).await?;
+    println!("{:?}", messages);
 
     HttpServer::new(|| {
         App::new().service(
