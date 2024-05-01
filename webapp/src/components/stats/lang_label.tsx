721ea993ae7ff8477b1f4211febb5d173afde86c
niooii <wangh3@bxscience.edu>
2024-05-01 09:05:53 -0400

    anims

--- webapp/src/components/stats/lang_label.tsx
+++
@@ -1,5 +0,0 @@
-export default function LangLabel() {
-    return (
-        <h1>hi chat</h1>
-    )
-}
--- webapp/src/components/tech_section_label.tsx
+++
@@ -1,9 +0,0 @@
-interface TechSectionLabelProps {
-    text: string
-}
-
-export default function TechSectionLabel({text}: TechSectionLabelProps) {
-    return (
-        <h1 className="font-semibold text-xl text-center">{text}</h1>
-    )
-}
